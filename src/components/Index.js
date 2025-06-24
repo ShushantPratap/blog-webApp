0259c11de307126e5ee757afa53f2e6cd41090ae
Shushant Pratap <sunnypratap912@gmail.com>
2025-06-24 18:19:49 +0530

    error solve

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,4 +1,4 @@
-import Header from './header/Header.jsx';
+import Header from './Header/Header';
 import Footer from './Footer/Footer';
 import Container from './container/Container';
 import LogoutBtn from './Header/LogoutBtn';
